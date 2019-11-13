6e6f95d35b5ec55d5df0c5333a1821aafb04cd46
Andrew Cobb <andrew@cobbs-web.com>
2019-11-13 13:40:00 -0500

    fixed webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
-    path: __dirname+ 'public'
+    path: __dirname+ '/public'
   },
   mode: devMode ? 'development' : 'production',
   module: {
